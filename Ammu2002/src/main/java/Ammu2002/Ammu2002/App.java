15f17e5013107d0e93b364ab967e8430c5ed62c0
91756 <91756@Anusha>
2022-12-23 12:48:23 +0530

    My frist commit

---
+++ Ammu2002/src/main/java/Ammu2002/Ammu2002/App.java
@@ -0,0 +1,13 @@
+package Ammu2002.Ammu2002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ammu2002/src/test/java/Sample3202/text123.java
@@ -0,0 +1,7 @@
+package Sample3202;
+
+public class text123<WebDriver> {
+	
+	WebDriver driver;
+
+}
